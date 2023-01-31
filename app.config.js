fba8f13da7d00c392a2dc660cc9c54f5c911cb15
benjamin <benjaminharringtonrose@gmail.com>
2023-01-31 14:39:08 -0500

    cleanup(env): fix env vars

--- app.config.js
+++ app.config.js
@@ -1,3 +1,5 @@
+const { parsed } = require('dotenv').config();
+
 module.exports = {
   "expo": {
     "name": "social-app",
@@ -30,13 +32,13 @@ module.exports = {
       "favicon": "./assets/favicon.png"
     },
     "extra": {
-      firebaseApiKey: process.env.FIREBASE_API_KEY,
-      firebaseAuthDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      firebaseProjectId: process.env.FIREBASE_PROJECT_ID,
-      firebaseStorageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      firebaseMessagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-      firebaseAppId: process.env.FIREBASE_APP_ID,
-      firebaseMeasurementId: process.env.FIREBASE_MEASUREMENT_ID
-    }
-  }
+      firebaseApiKey: parsed.FIREBASE_API_KEY,
+      firebaseAuthDomain: parsed.FIREBASE_AUTH_DOMAIN,
+      firebaseProjectId: parsed.FIREBASE_PROJECT_ID,
+      firebaseStorageBucket: parsed.FIREBASE_STORAGE_BUCKET,
+      firebaseMessagingSenderId: parsed.FIREBASE_MESSAGING_SENDER_ID,
+      firebaseAppId: parsed.FIREBASE_APP_ID,
+      firebaseMeasurementId: parsed.FIREBASE_MEASUREMENT_ID
+    },
+  },
 }
