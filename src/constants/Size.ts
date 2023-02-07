aec8973b49d8735eb999b1923932e0189293725c
DrySoldier <christiancotham61@gmail.com>
2023-02-07 12:46:21 -0500

    Scaling

--- src/constants/Size.ts
+++ src/constants/Size.ts
@@ -2,6 +2,12 @@ import { Dimensions } from 'react-native';
 
 const { width, height } = Dimensions.get('window');
 
+const guidelineBaseWidth = 350;
+
+const scale = (size: number) => (width / guidelineBaseWidth) * size;
+
+export const rs = (size: number, factor = 0.5) => size + (scale(size) - size) * factor;
+
 export const Size = {
   width,
   height,
