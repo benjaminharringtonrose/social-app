e9ff4919dd99861a28e8b093bc9bb72526c57a18
benjamin <benjaminharringtonrose@gmail.com>
2023-02-20 12:35:34 -0500

    cleanup

--- src/hooks/useMockRefresh.ts
+++ src/hooks/useMockRefresh.ts
@@ -1,14 +1,15 @@
 import { useState } from "react";
 
-export const useMockRefresh = () => {
+export const useMockRefresh = (ms?: number) => {
   const [refreshing, setRefreshing] = useState(false);
 
+  const milliseconds = ms || 1000;
+
   const onRefresh = () => {
-    console.log('triggered');
     setRefreshing(true);
     setTimeout(() => {
     setRefreshing(false);
-    }, 1000)
+    }, milliseconds)
   }
 
   return {
