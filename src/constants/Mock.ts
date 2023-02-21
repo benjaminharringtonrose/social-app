1fc26e50d456bf2b122e9427fce9889ebed96767
benjamin <benjaminharringtonrose@gmail.com>
2023-02-20 21:20:50 -0500

    feat(messages): cleanup

--- src/constants/Mock.ts
+++ src/constants/Mock.ts
@@ -3,7 +3,7 @@ export const Mock = {
   feedData: [
     {
       id: '0',
-      avatarUrl: 'https://picsum.photos/200/300',
+      avatarUrl: 'https://picsum.photos/id/111/200/300',
       name: 'John Smith',
       timestamp: '2 mins ago',
       caption: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
@@ -13,7 +13,7 @@ export const Mock = {
     },
     {
       id: '1',
-      avatarUrl: 'https://picsum.photos/200/300',
+      avatarUrl: 'https://picsum.photos/id/222/200/300',
       name: 'Julie Alexandra',
       timestamp: '12 mins ago',
       content: ['https://picsum.photos/200/300'],
@@ -22,7 +22,7 @@ export const Mock = {
     },
     {
       id: '2',
-      avatarUrl: 'https://picsum.photos/200/300',
+      avatarUrl: 'https://picsum.photos/id/331/200/300',
       name: 'Robert Wilkinson',
       timestamp: '13 mins ago',
       caption: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
@@ -31,7 +31,7 @@ export const Mock = {
     },
     {
       id: '3',
-      avatarUrl: 'https://picsum.photos/200/300',
+      avatarUrl: 'https://picsum.photos/id/444/200/300',
       name: 'Duante Williams',
       timestamp: '36 mins ago',
       caption: 'Excepteur sint occaecat cupidatat!!',
--- src/screens/MessagesScreen/index.tsx
+++ src/screens/MessagesScreen/index.tsx
@@ -1,25 +1,42 @@
 import React, { FC } from 'react';
-import { FlatList } from 'react-native';
+import { FlatList, RefreshControl, ListRenderItemInfo } from 'react-native';
+
 import { MessageThreadButton, RootView } from '../../components';
-import { Mock } from '../../constants';
+import { Color, Mock } from '../../constants';
+import { useMockRefresh, useOnFocusFadeIn } from '../../hooks';
+import { IContent } from '../../types';
 
 const MessagesScreen: FC = () => {
+
+  const { FadeIn, animatedStyle } = useOnFocusFadeIn();
+  const { onRefresh, refreshing } = useMockRefresh();
+
+  const renderItem = ({ item }: ListRenderItemInfo<IContent>) => (
+    <MessageThreadButton 
+      source={{ uri: item.avatarUrl }} 
+      name={item.name}
+      timestamp={item.timestamp}
+    />  
+  );
+  
   return (
     <RootView>
-      <FlatList
-        contentContainerStyle={{ marginTop: 50 }}
-        keyExtractor={(item) => item.id}
-        data={Mock.messageThreadData}
-        renderItem={({ item }) => {
-          return (
-            <MessageThreadButton 
-              source={{ uri: item.avatarUrl }} 
-              name={item.name}
-              timestamp={item.timestamp}
-            />          
-          )
-        }}
-      />
+      <FadeIn style={{ flex: 1 }} animatedStyle={animatedStyle}>
+        <FlatList
+          contentContainerStyle={{ marginTop: 50 }}
+          keyExtractor={(item) => item.id}
+          data={Mock.messageThreadData}
+          renderItem={renderItem}
+          refreshControl={
+            <RefreshControl 
+              refreshing={refreshing} 
+              tintColor={Color.white} 
+              progressViewOffset={50} 
+              onRefresh={onRefresh}  
+            />
+          }
+        />
+      </FadeIn>
     </RootView>
   );
 };    
