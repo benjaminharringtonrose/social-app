9783fe5c45dda059b42fba6532b8293102b95d02
benjamin <benjaminharringtonrose@gmail.com>
2023-02-20 19:56:00 -0500

    cleanup BottomTabBar

--- src/components/BottomTabBar/index.tsx
+++ src/components/BottomTabBar/index.tsx
@@ -2,30 +2,38 @@ import React, { useEffect, useState } from 'react';
 
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { View, TouchableOpacity, InteractionManager } from 'react-native';
+import { View, TouchableOpacity, InteractionManager, LayoutChangeEvent } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { BlurView } from 'expo-blur';
 import Reanimated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { BlurView } from 'expo-blur';
+import { orderBy } from 'lodash';
 
 import { Color, Size } from '../../constants';
+import { getIconName } from '../../utils';
 
 import styles from './styles';
 
+interface ITab {
+  width: number;
+  x: number;
+}
+
 const BottomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
   const indicatorOffset = useSharedValue(0);
+  const indicatorWidth = useSharedValue(Size.width / state.routeNames.length);
+
+  const [dimensions, setDimensions] = useState<ITab[]>([]);
 
   const { bottom } = useSafeAreaInsets();
 
-  const tabCount = state.routes?.length;
-  const tabWidth = Size.width / tabCount;
-
   const animStyle = useAnimatedStyle(() => ({
     transform: [{ translateX: withTiming(indicatorOffset.value) }],
-  }))
+    width: withTiming(indicatorWidth.value),
+  }), [dimensions]);
 
   return (
     <BlurView tint={'dark'} intensity={50} style={styles.blurView}>
-      <Reanimated.View style={[styles.tabIndicator, { width: tabWidth }, animStyle]} />
+      <Reanimated.View style={[styles.tabIndicator, animStyle]} />
       <View style={styles.container}>
         {state.routes.map((route, index) => {
           const { options } = descriptors[route.key];
@@ -37,7 +45,6 @@ const BottomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => 
               target: route.key,
               canPreventDefault: true,
             });
-
             if (!isFocused && !event.defaultPrevented) {
               navigation.navigate({ 
                 name: route.name,
@@ -45,35 +52,26 @@ const BottomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => 
                 merge: true,
               });
             }
-
             InteractionManager.runAfterInteractions(() => {
-              indicatorOffset.value = idx * tabWidth;
+              indicatorOffset.value = dimensions[idx].x;
+              indicatorWidth.value = dimensions[idx].width;
             });
           };
 
-          const getIconName = (routeName: string) => {
-            switch(routeName) {
-              case 'HomeScreen':
-                return isFocused ? 'home' : 'home-outline';
-              case 'MessagesScreen':
-                return isFocused ? 'mail' : 'mail-outline';
-              case 'PostScreen':
-                return isFocused ? 'add-circle' : 'add-circle-outline' ;
-              case 'NotificationsScreen':
-                return isFocused ? 'notifications' : 'notifications-outline';
-              case 'SettingsScreen':
-                return isFocused ? 'settings' : 'settings-outline';
+          const onLayout = (e: LayoutChangeEvent) => {
+            if (dimensions.length === state.routeNames.length) {
+              return;
             }
-          }
+            const { width, x } = e.nativeEvent.layout;
+            const sortedTabs = orderBy([...dimensions, { width, x }], 'x', 'asc');
+            setDimensions(sortedTabs);
+          };
 
           return (
-            <View key={index} style={[styles.iconContainer,  { paddingBottom: bottom }]}>
-              <TouchableOpacity
-                testID={options.tabBarTestID}
-                onPress={() => onPress(index)}
-              >
+            <View onLayout={onLayout} key={index} style={[styles.iconContainer,  { paddingBottom: bottom }]}>
+              <TouchableOpacity testID={options.tabBarTestID} onPress={() => onPress(index)}>
                 <Ionicons 
-                  name={getIconName(route.name)}
+                  name={getIconName(route.name, isFocused)}
                   size={25}
                   color={isFocused ? Color.teal : Color.white}
                 />
--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,3 +1,18 @@
 import { Platform } from "react-native";
 
 export const isIOS = () => Platform.OS === 'ios';
+
+export const getIconName = (routeName: string, isFocused: boolean) => {
+  switch(routeName) {
+    case 'HomeScreen':
+      return isFocused ? 'home' : 'home-outline';
+    case 'MessagesScreen':
+      return isFocused ? 'mail' : 'mail-outline';
+    case 'PostScreen':
+      return isFocused ? 'add-circle' : 'add-circle-outline' ;
+    case 'NotificationsScreen':
+      return isFocused ? 'notifications' : 'notifications-outline';
+    case 'SettingsScreen':
+      return isFocused ? 'settings' : 'settings-outline';
+  }
+}
