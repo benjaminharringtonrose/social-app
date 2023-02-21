02aeeb83a6239af90ec3912bea0458d328f86aaf
benjamin <benjaminharringtonrose@gmail.com>
2023-02-20 19:22:26 -0500

    cleanup BottomTabBar

--- src/components/BottomTabBar/index.tsx
+++ src/components/BottomTabBar/index.tsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import Ionicons from '@expo/vector-icons/Ionicons';
-import { View, TouchableOpacity } from 'react-native';
+import { View, TouchableOpacity, InteractionManager } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { BlurView } from 'expo-blur';
-import Reanimated, { useAnimatedStyle, useSharedValue, withTiming, WithTimingConfig, Easing } from 'react-native-reanimated';
+import Reanimated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 import { Color, Size } from '../../constants';
 
@@ -13,43 +13,31 @@ import styles from './styles';
 
 const BottomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => {
   const indicatorOffset = useSharedValue(0);
-  const [animate, triggerAnimation] = useState(false);
 
   const { bottom } = useSafeAreaInsets();
 
   const tabCount = state.routes?.length;
   const tabWidth = Size.width / tabCount;
 
-  const timingConfig: WithTimingConfig = {
-    duration: 250,
-    easing: Easing.linear
-  }
-
-  useEffect(() => {
-    indicatorOffset.value = withTiming(state.index * tabWidth, timingConfig);
-  }, [animate]);
-
-  const animStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: indicatorOffset.value }],
-    };
-  });
+  const animStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: withTiming(indicatorOffset.value) }],
+  }))
 
   return (
-    <BlurView tint="dark" intensity={50} style={styles.blurView}>
+    <BlurView tint={'dark'} intensity={50} style={styles.blurView}>
       <Reanimated.View style={[styles.tabIndicator, { width: tabWidth }, animStyle]} />
       <View style={styles.container}>
         {state.routes.map((route, index) => {
           const { options } = descriptors[route.key];
           const isFocused = state.index === index;
 
-          const onPress = () => {
-            triggerAnimation((_animate) => !_animate);
+          const onPress = (idx: number) => {
             const event = navigation.emit({
               type: 'tabPress',
               target: route.key,
               canPreventDefault: true,
             });
+
             if (!isFocused && !event.defaultPrevented) {
               navigation.navigate({ 
                 name: route.name,
@@ -57,12 +45,9 @@ const BottomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => 
                 merge: true,
               });
             }
-          };
 
-          const onLongPress = () => {
-            navigation.emit({
-              type: 'tabLongPress',
-              target: route.key,
+            InteractionManager.runAfterInteractions(() => {
+              indicatorOffset.value = idx * tabWidth;
             });
           };
 
@@ -82,19 +67,18 @@ const BottomTabBar = ({ state, descriptors, navigation }: BottomTabBarProps) => 
           }
 
           return (
-          <View key={index} style={[styles.iconContainer,  { paddingBottom: bottom }]}>
-            <TouchableOpacity
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              onLongPress={onLongPress}
-            >
-              <Ionicons 
-                name={getIconName(route.name)}
-                size={25}
-                color={isFocused ? Color.teal : Color.white}
-              />
-            </TouchableOpacity>
-             </View>
+            <View key={index} style={[styles.iconContainer,  { paddingBottom: bottom }]}>
+              <TouchableOpacity
+                testID={options.tabBarTestID}
+                onPress={() => onPress(index)}
+              >
+                <Ionicons 
+                  name={getIconName(route.name)}
+                  size={25}
+                  color={isFocused ? Color.teal : Color.white}
+                />
+              </TouchableOpacity>
+            </View>
           );
         })}
       </View>
